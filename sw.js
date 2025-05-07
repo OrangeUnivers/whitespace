9c865176eacd67089a28f5964f8639f1cb12b1d0
OrangeUnivers <118116610+OrangeUnivers@users.noreply.github.com>
2025-05-07 23:35:05 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+// self.addEventListener('install', (event) => {
+//   console.log('Service Worker installed');
+// });
+
+// self.addEventListener('activate', (event) => {
+//   console.log('Service Worker activated');
+// });
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        'index.html',
+        '/icons/ws/icon-small.png',
+        '/icons/ws/icon-large.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
